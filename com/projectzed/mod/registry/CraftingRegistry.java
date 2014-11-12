47f30018538226d76df12ed4ffdad665ee9e0f7e
Nick Hurd <datcodplaya@gmail.com>
2014-11-12 13:47:19 -0500

    - Added jdoc to crafting registry.

--- com/projectzed/mod/registry/CraftingRegistry.java
+++ com/projectzed/mod/registry/CraftingRegistry.java
@@ -28,42 +28,91 @@ public class CraftingRegistry {
 		shapedList = new ArrayList<ShapedOreRecipe>();
 	}
 	
+	/**
+	 * Get the instance of this class in its static way.
+	 * @return static instance of this class.
+	 */
 	public static CraftingRegistry instance() {
 		return reg;
 	}
 	
+	/**
+	 * Get the shapeless crafting list created during init method call.
+	 * @return shapeless crafting list.
+	 */
 	public List<ShapelessOreRecipe> getShapelessList() {
 		return reg.shapelessList;
 	}
 	
+	/**
+	 * Get the shaped crafting list created during the init method call.
+	 * @return shaped crafting list.
+	 */
 	public List<ShapedOreRecipe> getShapedList() {
 		return reg.shapedList;
 	}
 	
+	/**
+	 * Method once called, will handle the initializing of all crafting recipes.
+	 */
 	public void init() {
 		initShapeless();
 		initShaped();
 	}
 	
+	/**
+	 * Sub-init method for init'ing shapeless crafting recipes into list.
+	 */
 	private void initShapeless() {
 		reg.shapelessList.add(createShapelessRecipe(ProjectZed.energyPipe, 8, "ingotTitanium", Items.redstone, "ingotTitanium"));
 	}
 	
+	/**
+	 * Sub-init method for init'ing shapeless crafting recipes into list.
+	 */
 	private void initShaped() {
 	}
 	
+	/**
+	 * Method used for creating a new shapeless crafting recipes.
+	 * @param item = item output from said recipe.
+	 * @param amount = amount of items to receive.
+	 * @param objects = array of objects for how crafting recipe essentially should look.
+	 * @return created shapeless recipe object if successful, else return null.
+	 */
 	private static ShapelessOreRecipe createShapelessRecipe(Item item, int amount, Object... objects) {
 		return item == null || objects == null || objects.length < 1 ? null : new ShapelessOreRecipe(new ItemStack(item, amount), objects);
 	}
 	
+	/**
+	 * Method used for creating a new shapeless crafting recipes.
+	 * @param block = block output from said recipe.
+	 * @param amount = amount of items to receive.
+	 * @param objects = array of objects for how crafting recipe essentially should look.
+	 * @return created shapeless recipe object if successful, else return null.
+	 */
 	private static ShapelessOreRecipe createShapelessRecipe(Block block, int amount, Object... objects) {
 		return block == null || objects == null || objects.length < 1 ? null : new ShapelessOreRecipe(new ItemStack(block, amount), objects);
 	}
 	
+	/**
+	 * Method used for creating a new shaped crafting recipe.
+	 * @param item = item output from said recipe.
+	 * @param amount = amount of items to receive.
+	 * @param objects = array of objects for how crafting recipe essentially should look.
+	 * @return created shaped recipe object if successful, else return null.
+	 */
 	private static ShapedOreRecipe createShapedRecipe(Item item, int amount, Object... objects) {
 		return item == null || objects == null || objects.length < 4 ? null : new ShapedOreRecipe(new ItemStack(item, amount), objects);
 	}
 	
+	/**
+	 * Method used for creating a new shaped crafting recipe.
+	 * @param block = block output from said recipe.
+	 * @param amount = amount of items to receive.
+	 * @param objects = array of objects for how crafting recipe essentially should look.
+	 * @return created shaped recipe object if successful, else return null.
+	 */
 	private static ShapedOreRecipe createShapedRecipe(Block block, int amount, Object... objects) {
 		return block == null || objects == null || objects.length < 4 ? null : new ShapedOreRecipe(new ItemStack(block, amount), objects);
 	}
